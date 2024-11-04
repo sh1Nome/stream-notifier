b7937da51b46cfcf5bab21ce619c81ca989e8013
sh1Nome <shinodayo1024@gmail.com>
2024-11-05 00:37:55 +0900

    [update]

---
+++ work/app/src/main/resources/db/migration/V1__setting.sql
@@ -0,0 +1,11 @@
+CREATE TABLE lang (
+    guild_id VARCHAR(20) NOT NULL,
+    lang VARCHAR(20) NOT NULL,
+    PRIMARY KEY(guild_id)
+);
+
+CREATE TABLE notify (
+    guild_id VARCHAR(20) NOT NULL,
+    channel_id VARCHAR(20) NOT NULL,
+    PRIMARY KEY(guild_id)
+);
