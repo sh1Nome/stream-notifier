d80a73aa680ea998073d3b346c58ed8891308284
sh1Nome <shinodayo1024@gmail.com>
2024-12-14 22:54:47 +0900

    update

--- work/app/src/main/java/com/re_kid/discordbot/StreamNotifier.java
+++ work/app/src/main/java/com/re_kid/discordbot/StreamNotifier.java
@@ -1,34 +1,8 @@
 package com.re_kid.discordbot;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.re_kid.discordbot.command.lang.Lang;
-import com.re_kid.discordbot.listener.StreamNotifierEventListener;
-
-import net.dv8tion.jda.api.JDA;
-
-/**
- * メインクラス
- */
 public class StreamNotifier {
 
-    private final JDA api;
-    private final StreamNotifierEventListener streamNotifierEventListener;
-    private final Lang lang;
-
-    public StreamNotifier(JDA api, StreamNotifierEventListener streamNotifierEventListener, Lang lang) {
-        this.api = api;
-        this.streamNotifierEventListener = streamNotifierEventListener;
-        this.lang = lang;
-    }
-
-    public void run() {
-        this.lang.changeActivity();
-        this.api.addEventListener(this.streamNotifierEventListener);
-    }
-
     public static void main(String[] args) {
-        Injector injector = Guice.createInjector(new StreamNotifierModule());
-        injector.getInstance(StreamNotifier.class).run();
+        System.out.println("debug");
     }
 }
