87667912f55d8e5ad14a887b068ef18d9512d66b
sh1Nome <shinodayo1024@gmail.com>
2024-12-19 00:15:40 +0900

    update

--- work/app/src/main/java/com/re_kid/discordbot/presentation/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.re_kid.discordbot.presentation;
-
-import java.util.List;
-import java.util.Map;
-
-public interface Controller {
-    public void execute(Map<String,List<String>> optionAndArgs);
-}
